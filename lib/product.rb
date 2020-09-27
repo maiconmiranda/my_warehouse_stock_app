6aac71e69826c15f9fe1dd388c1127b37a9ee41f
MaiconMiranda <alves_maicon@rocketmail.com>
2020-09-28 09:49:57 +1000

    general modifications to include classes between files

--- lib/product.rb
+++ lib/product.rb
@@ -1,23 +1,27 @@
-class Products
+# frozen_string_literal: true
 
-  INPUTS = %i[product_name category quantity quantity_per_box item_weight]
+require_relative 'product_helper'
 
-  def initialize(id, product_name, category, quantity, quantity_per_box, item_weight)
+class Product
+  include ProductHelper
+
+  attr_reader :id, :product, :category, :quantity, :quantity_per_box, :item_weight, :box_weight
+
+  def initialize(id, product, category, quantity, quantity_per_box, item_weight, box_weight)
     @id = id
-    @product_name = product_name
+    @product = product
     @category = category
     @quantity = quantity
     @quantity_per_box = quantity_per_box
     @item_weight = item_weight
-    @box_weight = item_weight * quantity_per_box
+    @box_weight = box_weight
   end
-
 
   def to_a
-    [@id, @product_name, @category, @quantity, @quantity_per_box, @item_weight, @box_weight]
+    [@id, @product, @category, @quantity, @quantity_per_box, @item_weight, @box_weight]
   end
 
-  def self.products_user_input
+  def self.product_user_input
     product = {}
     INPUTS.each do |input|
       puts "Whats the #{input}"
@@ -27,4 +31,3 @@ class Products
     product
   end
 end
-
---
+++ lib/run.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative 'product'
+require_relative 'product_helper'
+require_relative 'product_repository'
