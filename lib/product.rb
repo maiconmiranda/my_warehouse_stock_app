40f2956025f09c8488d8e45e47318b7ee3768b36
MaiconMiranda <alves_maicon@rocketmail.com>
2020-09-28 18:54:42 +1000

    defined method to multiply item weight with quantity per box to return box weight

--- lib/product.rb
+++ lib/product.rb
@@ -3,22 +3,37 @@
 require_relative 'product_helper'
 
 class Product
+  attr_accessor :id, :product, :category, :quantity, :quantity_per_box, :item_weight, :box_weight
+
   include ProductHelper
 
-  attr_reader :id, :product, :category, :quantity, :quantity_per_box, :item_weight, :box_weight
-
-  def initialize(id, product, category, quantity, quantity_per_box, item_weight, box_weight)
+  def initialize(id, product, category, quantity, quantity_per_box, item_weight, _box_weight = 0)
     @id = id
     @product = product
     @category = category
     @quantity = quantity
     @quantity_per_box = quantity_per_box
     @item_weight = item_weight
-    @box_weight = box_weight
+    @box_weight = box_weight_sum
+    # @in = In
+    # @out = Out
   end
 
   def to_a
     [@id, @product, @category, @quantity, @quantity_per_box, @item_weight, @box_weight]
+  end
+
+  def box_weight_sum
+    num1 = @quantity_per_box
+    num2 = @item_weight
+    sum = num1 * num2
+    sum
+  end
+
+  def increase_quantity
+    puts 'How much you would like to increase the quantity?'
+    increment = gets.chomp.to_i
+    @quantity += increment
   end
 
   def self.product_user_input
