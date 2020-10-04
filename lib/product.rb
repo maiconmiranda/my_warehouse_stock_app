84760c5928d3d6f3adc91905b8be3ca7d8bae26b
MaiconMiranda <alves_maicon@rocketmail.com>
2020-10-04 18:10:10 +1100

    changed list display view using tty-table gem and user prompt

--- lib/product.rb
+++ lib/product.rb
@@ -2,8 +2,9 @@
 
 require_relative 'product_helper'
 
+# class that create all the instances for this app
 class Product
-  attr_accessor :id, :product, :category, :quantity, :quantity_per_box, :item_weight, :box_weight
+  attr_reader :id, :product, :category, :quantity, :quantity_per_box, :item_weight, :box_weight
 
   include ProductHelper
 
@@ -15,34 +16,38 @@ class Product
     @quantity_per_box = quantity_per_box
     @item_weight = item_weight
     @box_weight = box_weight_sum
-    # @in = In
-    # @out = Out
   end
 
   def to_a
     [@id, @product, @category, @quantity, @quantity_per_box, @item_weight, @box_weight]
   end
 
+  # returns the total value for box_weight
   def box_weight_sum
     num1 = @quantity_per_box
     num2 = @item_weight
     sum = num1 * num2
-    sum
+    sum.round(2)
   end
 
-  def increase_quantity
-    puts 'How much you would like to increase the quantity?'
-    increment = gets.chomp.to_i
-    @quantity += increment
+  def increment(value)
+    @quantity += value
+  end
+
+  def decrease(value)
+    @quantity -= value
   end
 
   def self.product_user_input
-    product = {}
-    INPUTS.each do |input|
-      puts "Whats the #{input}"
-      print '> '
-      product[input] = gets.chomp
+    prompt = PROMPT_ADD
+    product = prompt.collect do
+      key(:product).ask('Type the product name: ')
+      key(:category).ask('Type the category: ')
+      key(:quantity).ask('Type the quantity: ', convert: :int)
+      key(:quantity_per_box).ask('Type the quantity_per_box: ', convert: :float)
+      key(:item_weight).ask('Type the item_weight: ', convert: :float)
     end
     product
   end
 end
+
