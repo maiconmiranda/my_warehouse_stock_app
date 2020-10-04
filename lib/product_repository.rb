f6bce8633bda2cb2e8c29be7d8f0fbec93f9b606
MaiconMiranda <alves_maicon@rocketmail.com>
2020-10-04 18:08:02 +1100

    added confirmation box for edit menu and small updates in the edit methods

--- lib/product_repository.rb
+++ lib/product_repository.rb
@@ -2,15 +2,15 @@
 
 require 'tty-prompt'
 require 'json'
-require 'terminal-table'
+require 'tty-box'
 require 'pastel'
 require 'tty-table'
-require 'colorize'
 require_relative 'product_helper'
 require_relative 'product'
 
+# class contains methods to work with json file, also delete and edit options
 class ProductFile
-  attr_accessor :products
+  attr_reader :products
 
   include ProductHelper
 
@@ -19,7 +19,7 @@ class ProductFile
   end
 
   def read_products
-    data = FILE_READ
+    data = File.read(PRODUCT_DATABASE)
     JSON.parse(data).map do |product|
       Product.new(
         product['id'],
@@ -54,65 +54,88 @@ class ProductFile
       @products.length + 1,
       product[:product],
       product[:category],
-      product[:quantity].to_i,
-      product[:quantity_per_box].to_f,
-      product[:item_weight].to_f
+      product[:quantity],
+      product[:quantity_per_box],
+      product[:item_weight],
+      product[:box_weight]
     )
   end
 
-  def set_product_names
-    product_names = @products.map do |product|
-      product.product
+   # display confirmation message after delete
+   def box_del_confirmation
+    print TTY::Cursor.clear_screen
+    box_del = TTY::Box.frame(
+      top: 2,
+      left: 2,
+      width: 50,
+      height: 5,
+      border: :thick,
+      align: :center,
+      padding: 1,
+      title: {
+        top_left: ' success '
+      },
+      style: {
+        fg: :white,
+        bg: :red,
+        border: {
+          fg: :bright_yellow,
+          bg: :red
+        }
+      }
+    ) do
+      'Product deleted'
     end
-    product_names
+    puts box_del
   end
 
-  def select_delete
-    index = FILE_READ
-    delete_prompt = PROMPT_DELETE
-    choice = delete_prompt.select('Which Product you want to delete?', set_product_names) do |menu|
+  # gets the product name to display in tty-prompt for edit and delete options
+  def product_name
+    name = []
+    @products.map do |product|
+      name << product.product
+    end
+    name
+  end
+
+  # gets user's input and return array index of the selected choice for edit option
+  def selection_to_edit
+    delete_prompt = PROMPT_ADD
+    choice = delete_prompt.select('Which Product you want to edit?', product_name) do |menu|
       menu.enum '.'
     end
-    product_index = @products.index { |h| h.product == choice }
-    p product_index
+    @products.index { |h| h.product == choice }
+  end
 
-    hash = @products.map do |product|
-      {
-        'id' => product.id,
-        'product' => product.product,
-        'category' => product.category,
-        'quantity' => product.quantity,
-        'quantity_per_box' => product.quantity_per_box,
-        'item_weight:' => product.item_weight,
-        'box_weight' => product.box_weight
-      }
-    end
-    list = hash[product_index]
-
-    key = []
-    row = []
-    list.each_pair do |k, v|
-      key << k.colorize(:yellow)
-      row << v.to_s.colorize(:red)
-    end
-    table = TTY::Table.new
-    table << key
-    table << row
-    puts table.render(:unicode, alignments: :center, padding: [1, 2, 1, 2]) do |renderer|
-      renderer.border.separator = :each_row
-    end
-
+  # confirms delete option and delete a selected hash by index
+  def confirm_and_delete
+    selected = selection_to_edit
+    p selected
     prompt = PROMPT_DELETE
     result = prompt.yes?('Are you sure you want delete?')
     if result
-      @products.delete_at(product_index)
-      write_product
-      manage_menu
+      @products.delete_at(selected)
     else
       manage_menu
     end
   end
 
+  # confirms edit option, delete select and create a new product
+  def replace_product
+    edit_selected = selection_to_edit
+    p edit_selected
+    prompt = PROMPT_DELETE
+    result = prompt.yes?('Are you sure you want edit product?')
+    if result
+      @products.delete_at(edit_selected)
+      create_product
+      write_product
+    else
+      manage_menu
+    end
+  end
+
+  # edit menu
   def manage_menu
     prompt2 = PROMPT_DELETE
     prompt2.select('Choose one option below') do |menu|
@@ -126,13 +149,19 @@ class ProductFile
     loop do
       case manage_menu
       when '1'
-        puts 'not available'
+        replace_product
+        box_edit_confirmation
+        write_product
         MainMenu.new.router
       when '2'
-        select_delete
+        confirm_and_delete
+        box_del_confirmation
+        write_product
+        MainMenu.new.router
       when '3'
         MainMenu.new.router
       end
     end
   end
+
 end
