93583805434e6b96287220899b426a07078e35aa
MaiconMiranda <alves_maicon@rocketmail.com>
2020-10-04 18:25:55 +1100

    added menu logic for in and out and methods for in and out

--- lib/product_repository.rb
+++ lib/product_repository.rb
@@ -61,8 +61,8 @@ class ProductFile
     )
   end
 
-   # display confirmation message after delete
-   def box_del_confirmation
+  # display confirmation message after delete
+  def box_del_confirmation
     print TTY::Cursor.clear_screen
     box_del = TTY::Box.frame(
       top: 2,
@@ -87,6 +87,62 @@ class ProductFile
       'Product deleted'
     end
     puts box_del
+  end
+
+  # display confirmation message after edit
+  def box_edit_confirmation
+    print TTY::Cursor.clear_screen
+    box_edit = TTY::Box.frame(
+      top: 3,
+      left: 3,
+      width: 80,
+      height: 5,
+      border: :thick,
+      align: :center,
+      padding: 1,
+      title: {
+        top_left: ' success '
+      },
+      style: {
+        fg: :black,
+        bg: :green,
+        border: {
+          fg: :black,
+          bg: :green
+        }
+      }
+    ) do
+      'The product you edited will be displayed at the end of the list'
+    end
+    puts box_edit
+  end
+
+  # display confirmation message after add or remove quantity
+  def box_in_out_confirmation
+    print TTY::Cursor.clear_screen
+    box_in_out = TTY::Box.frame(
+      top: 3,
+      left: 5,
+      width: 50,
+      height: 5,
+      border: :thick,
+      align: :center,
+      padding: 1,
+      title: {
+        top_left: ' success '
+      },
+      style: {
+        fg: :white,
+        bg: :blue,
+        border: {
+          fg: :white,
+          bg: :blue
+        }
+      }
+    ) do
+      'Quantity changed'
+    end
+    puts box_in_out
   end
 
   # gets the product name to display in tty-prompt for edit and delete options
@@ -115,9 +171,11 @@ class ProductFile
     result = prompt.yes?('Are you sure you want delete?')
     if result
       @products.delete_at(selected)
+      box_del_confirmation
     else
       manage_menu
     end
+    
   end
 
   # confirms edit option, delete select and create a new product
@@ -130,9 +188,67 @@ class ProductFile
       @products.delete_at(edit_selected)
       create_product
       write_product
+      box_edit_confirmation
     else
       manage_menu
     end
+    
+  end
+
+  # gets user's input and return array index of the selected choice for in and out option
+  def selection_for_in_out
+    delete_prompt = PROMPT_IN_OUT
+    choice = delete_prompt.select('Which Product you want add/remove quantity?', product_name) do |menu|
+      menu.enum '.'
+    end
+    @products.index { |h| h.product == choice }
+  end
+
+  # gets user input for in and out
+  def in_new
+    prompt = PROMPT_IN_OUT
+    in_qty = prompt.collect do
+      key(:in_out_qty).ask('Waht is the quantity to be added? ', convert: :integer)
+    end
+    in_qty
+  end
+
+  # increase quantity fo In option
+  def increase_quantity
+    @products[selection_for_in_out].increment(in_new[:in_out_qty])
+    write_product
+  end
+
+  # decrease quantity fo In option
+  def remove_quantity
+    @products[selection_for_in_out].decrease(in_new[:in_out_qty])
+  end
+
+  # confirmation for In option
+  def in_qty_add
+    prompt = PROMPT
+    result = prompt.yes?('Are you sure you want add quantity?')
+    if result
+      increase_quantity
+      box_in_out_confirmation
+    else
+      in_out_menu
+    end
+    
+  end
+
+  # confirmation for Out option
+  def out_qty_remove
+    prompt = PROMPT_DELETE
+    result = prompt.yes?('Are you sure you want remove quantity?')
+    if result
+      remove_quantity
+      write_product
+      box_in_out_confirmation
+    else
+      in_out_menu
+    end
+    
   end
 
   # edit menu
@@ -145,17 +261,24 @@ class ProductFile
     end
   end
 
+  def in_out_menu
+    prompt2 = PROMPT_IN_OUT
+    prompt2.select('Choose one option below') do |menu|
+      menu.choice({ name: 'In', value: '1' })
+      menu.choice({ name: 'Out', value: '2' })
+      menu.choice({ name: 'Back to main menu', value: '3' })
+    end
+  end
+
   def menu_selection
     loop do
       case manage_menu
       when '1'
         replace_product
-        box_edit_confirmation
         write_product
         MainMenu.new.router
       when '2'
         confirm_and_delete
-        box_del_confirmation
         write_product
         MainMenu.new.router
       when '3'
@@ -164,4 +287,18 @@ class ProductFile
     end
   end
 
+  def in_out_selection
+    loop do
+      case in_out_menu
+      when '1'
+        in_qty_add
+        MainMenu.new.router
+      when '2'
+        out_qty_remove
+        MainMenu.new.router
+      when '3'
+        MainMenu.new.router
+      end
+    end
+  end
 end
